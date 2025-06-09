5f9dbb55b20251b91beada71dac40bd89b4f4944
hyosub <sube72@gmail.com>
2025-06-09 14:49:28 +0900

    feat: font와 theme 추가

---
+++ lib/core/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sigbang/core/constants.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: kYellowColor,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: AppBarTheme(
+      backgroundColor: kYellowColor,
+    ),
+  );
+}
