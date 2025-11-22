f8c4ae96527300647dda7d59bec41c73aab1df7e
hyosub <sube72@gmail.com>
2025-11-23 07:06:09 +0900

    fix:  이 페이지는 SSR로만 렌더되어 빌드 시 프리렌더링을 건너

--- sigbang_user_front/src/app/account/delete/page.tsx
+++ sigbang_user_front/src/app/account/delete/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+
 import { useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 import Topbar from "@/components/Topbar";
