8522568db23f22f32345c6fe39b3040bd49dedc5
hyosub <sube72@gmail.com>
2025-11-22 19:53:14 +0900

    chore: ci error

---
+++ sigbang_user_front/open-next.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "open-next/config";
+
+export default defineConfig({
+  // Output directory used by the Cloudflare adapter and wrangler.toml
+  outDir: ".open-next",
+});
+
+
